8af45762161f9bd84615c9a1afcbb142202494ea
PKcodebase <kaushikprasad1659@gmail.com>
2026-01-27 22:49:11 +0530

    SortedSquare

---
+++ Array/src/com/array/SquareSortedArray.java
@@ -0,0 +1,34 @@
+package com.array;
+
+import java.util.Arrays;
+
+public class SquareSortedArray {
+    public static int[] sortedSquares(int[] nums) {
+
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int left = 0, right = n - 1;
+        int index = n - 1;
+
+        while (left <= right) {
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                result[index--] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[index--] = nums[right] * nums[right];
+                right--;
+            }
+        }
+
+        return result;
+    }
+
+    public static  void main(String[] args){
+        int[] nums={-4,-1,0,3,10};
+        int[] result = sortedSquares(nums);
+
+        // Print output
+        System.out.println(Arrays.toString(result));
+    }
+}
