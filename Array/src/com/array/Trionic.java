73d7fcc2644b4a927f3878231d83ad86ae70e63a
PKcodebase <kaushikprasad1659@gmail.com>
2026-02-03 11:10:58 +0530

    Trionic

---
+++ Array/src/com/array/Trionic.java
@@ -0,0 +1,31 @@
+package com.array;
+
+public class Trionic {
+    public static  boolean isTrionic(int[] nums){
+        int n = nums.length;
+        if(n< 4) return false;
+        int i = 1;
+        while(i<n && nums[i]>nums[i-1]){
+          i++;
+        }
+        if(i==1 || i==n){
+            return false;
+        }
+        while(i<n && nums[i]<nums[i]-1){
+            i++;
+        }
+        if(i==n){
+            return false;
+        }
+        while(i<n && nums[i]>nums[i]-1){
+            i++;
+        }
+        return i== n;
+
+    }
+    public static void main(String[] args){
+        int[] nums = {1,1,5,4,2,6,6};
+        boolean result = isTrionic(nums);
+        System.out.println(result);
+    }
+}
