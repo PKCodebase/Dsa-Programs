384618ea105b66c645dc1f92a2a740bc8113fff5
PKcodebase <kaushikprasad1659@gmail.com>
2026-02-12 22:57:00 +0530

    TwoSum

--- Array/src/com/array/TwoSum.java
+++ Array/src/com/array/TwoSum.java
@@ -18,7 +18,7 @@ public class TwoSum {
     }
 
     public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
+        int[] nums = {2, 9, 11, 15};
         int target = 9;
         int[] result = twoSum(nums, target);
         if (result.length == 2) {
